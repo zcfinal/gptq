5163a3c89649a3a091e431960bc025f8f00ae94b
张超（实习） <zhangchao2@xiaohongshu.com>
2025-01-05 21:25:51 +0800

    debug

--- run_exp.sh
+++ run_exp.sh
@@ -4,8 +4,14 @@
 
 # CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-125m c4 --wbits 4 --groupsize 1024
 
-CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-350m c4
+# CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-350m c4
 
-CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-350m c4 --wbits 4 --nearest
+# CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-350m c4 --wbits 4 --nearest
 
-CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-350m c4 --wbits 4 --groupsize 1024
+# CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-350m c4 --wbits 4 --groupsize 1024
+
+CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-1.3b c4
+
+CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-1.3b c4 --wbits 4 --nearest
+
+CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-1.3b c4 --wbits 4 --groupsize 1024
